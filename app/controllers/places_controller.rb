4b638e34977ef6af76f603c17b1fad6745be5108
Lindsay Dages <lindsaykayedutton@gmail.com>
2017-01-27 22:58:33 +0000

    restricted incorrect users to edit/update places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @places = Place.order("created_at ASC").all.paginate(page: params[:page], per_page: 10)
@@ -20,10 +20,16 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render text: "Not Allowed", status: :forbidden
+    end 
   end 
 
   def update
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render text: "Not Allowed", status: :forbidden
+    end 
     @place.update_attributes(place_params)
     redirect_to root_path
   end
